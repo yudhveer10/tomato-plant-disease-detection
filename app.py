0d429da36b8cb963ec311082cf9f3ff9c1afb27e
Dhruv Dubey <simplycode07@gmail.com>
2024-05-10 01:13:56 +0530

    Added flask, fixed links to htmls and stylesheets

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template
+import model
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/predict_image", method=["POST"])
+def predict_image():
+    # yaha prediction wala bna lena
+    prediction = "l"
+
+    return prediction
+
+app.run(debug=True, port=3000)
